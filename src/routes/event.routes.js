5019859ad34a080158e422ae16b2a32923c5ebbb
Sergio Ramírez <sergio_esfm02@hotmail.com>
2025-09-05 00:09:58 -0600

    Fix: arreglando error en la ruta de la función de expressValidators.js

--- src/routes/event.routes.js
+++ src/routes/event.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { validateBody } from '../middlewares/validate.js';
 import { createEventSchema } from '../validators/event.schema.js';
-import { validateMongoIdParam } from '../validators/expressValidators.js';
+import { validateMongoIdParam } from "../validators/expressValidators.js";
 import * as EventCtrl from '../controllers/event.controller.js';
 import { requireAuth, requireRole } from '../middlewares/auth.js';
 
